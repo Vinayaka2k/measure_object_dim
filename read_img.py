ec5c161f6ba71035412301731056892639735e22
Vinayaka2k <vinuvinayaka2000@gmail.com>
2023-12-17 07:14:16 +0530

    display images using matplotlib

--- read_img.py
+++ read_img.py
@@ -1,8 +1,22 @@
 import cv2
+import matplotlib.pyplot as plt
+
 img = cv2.imread("./cup_image.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(gray, 127, 155, 0)
+# plt.imshow(gray, cmap="gray")
+# plt.title('Grayscale Image')
+# plt.show()
+
+ret,thresh = cv2.threshold(gray, 127, 255, 0)
+# plt.imshow(thresh, cmap="gray")
+# plt.title('Thresholded Image')
+# plt.show()
+
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-print(contours)
-cv2.drawContours(img, contours, -1, (0,0,255), 2)
-cv2.imwrite('res.jpeg', img)
+contour_img = img.copy()
+cv2.drawContours(contour_img, contours, -1, (0,255,0), 2)
+
+plt.imshow(contour_img)
+plt.title('Image with Contours')
+plt.show()
+# cv2.imwrite('res.jpeg', img)
